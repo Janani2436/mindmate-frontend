739774ad24c75a58e5388b98bca9c9117382c674
Janani A <janania2436@gmail.com>
2025-06-14 14:34:52 +0530

    fix: update API base URL

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,10 +1,10 @@
 // client/src/api/axios.js
 import axios from 'axios';
 
-const instance = axios.create({
+const axiosInstance = axios.create({
   baseURL: process.env.REACT_APP_API_BASE_URL,
+  withCredentials: true,
 });
-
 
 instance.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
