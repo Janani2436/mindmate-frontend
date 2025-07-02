25e603e6bad17d00c796d07f7fd95ec90ad09d46
Janani A <janania2436@gmail.com>
2025-07-02 22:55:43 +0530

    🐛 Fix jwt-decode import (use named export)

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,6 +1,7 @@
 import { createContext, useContext, useEffect, useState, useMemo } from 'react';
 import { useNavigate } from 'react-router-dom';
-import jwtDecode from 'jwt-decode';
+import { jwtDecode } from 'jwt-decode';
+
 
 // 1. Create the Auth Context
 const AuthContext = createContext();
